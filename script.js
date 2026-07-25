34f4667a9763c9a7d5970068f628c8858bc599c2
sanjai k <sanjaikaids2007@gmail.com>
2026-07-25 22:37:47 +0530

    Add welcome message and resume button functionality

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// Smooth welcome message
+window.onload = function () {
+    console.log("Welcome to Sanjai's Portfolio!");
+};
 
+// Download Resume Button
+const button = document.querySelector("button");
+
+if (button) {
+    button.addEventListener("click", () => {
+        alert("Resume will be added soon!");
+    });
+}
